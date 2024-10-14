838c7c02d30846bdfb122dfd90cda8496097e4d7
이지윤A <wldbs272@naver.com>
2024-10-14 12:38:18 +0900

    Remove: unused component

--- Assets/Cainos/Pixel Art Top Down - Basic/Script/TopDownAimRotaion.cs
+++ Assets/Cainos/Pixel Art Top Down - Basic/Script/TopDownAimRotaion.cs
@@ -3,9 +3,6 @@ using UnityEngine;
 
 public class TopDownAimRotaion : MonoBehaviour
 {
-    [SerializeField] private SpriteRenderer armRenderer;
-    [SerializeField] private Transform armPivot;
-
     [SerializeField] private SpriteRenderer characterRenderer;
 
     private TopDownController controller;
@@ -30,6 +27,5 @@ public class TopDownAimRotaion : MonoBehaviour
         float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf .Rad2Deg;
 
         characterRenderer.flipX = Mathf.Abs(rotZ) > 90f;
-        armPivot.rotation = Quaternion.Euler(0,0, rotZ);
     }
 }
